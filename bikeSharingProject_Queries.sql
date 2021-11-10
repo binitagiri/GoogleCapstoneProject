c25f45cda418afe77f7f9614eecb35ddf5d3c3e8
BINITA GIRISH <68512904+binitagiri@users.noreply.github.com>
2021-11-09 16:14:08 -0800

    Update bikeSharingProject_Queries.sql

--- bikeSharingProject_Queries.sql
+++ bikeSharingProject_Queries.sql
@@ -80,6 +80,36 @@ SELECT TOP (1000) [ride_id]
 SELECT *
   FROM [BikeShareProject].[dbo].[Yearly_Data_Sep2020_Aug2021]
 
+  /****** Script for SelectTopNRows command from SSMS for records whose ended_at<started_at ******/
+SELECT count(datediff(minute,started_at,ended_at))
+FROM [BikeShareProject].[dbo].[Yearly_Data_Sep2020_Aug2021] where datediff(minute,started_at,ended_at)<0 
+
+/* delete invalid records of 2285 whose ended_at < started_at*/
+
+delete from [BikeShareProject].[dbo].[Yearly_Data_Sep2020_Aug2021] 
+where datediff(minute,started_at,ended_at)<0 
+
+/****** check whether ride-id is unique , it is ******/
+SELECT distinct(ride_id),count(*) as ridecount
+  FROM [BikeShareProject].[dbo].[Yearly_Data_Sep2020_Aug2021] group by ride_id order by ridecount desc
+
+  
+/****** check whether rideable_type is unique , it is ******/
+SELECT distinct(rideable_type),count(*) as ridecount
+  FROM [BikeShareProject].[dbo].[Yearly_Data_Sep2020_Aug2021] group by rideable_type order by ridecount desc
+
+  /****** check whether started_at or ended_at is not null ******/
+SELECT count(*) 
+  FROM [BikeShareProject].[dbo].[Yearly_Data_Sep2020_Aug2021]  where started_at is null or ended_at is null
+
+
+    /****** add a table for ride-length ******/
+  ALTER TABLE [BikeShareProject].[dbo].[Yearly_Data_Sep2020_Aug2021]
+   ADD [RIDE_LENGTH] float
+
+   	 /*add a column with minutes of ride to the yearly trip data*/
+   UPDATE [BikeShareProject].[dbo].[Yearly_Data_Sep2020_Aug2021] SET RIDE_LENGTH=datediff(minute,started_at,ended_at)
+
 
   /*total no:of rides from sep 2020 to aug 2021*/
 
